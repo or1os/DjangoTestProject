f489a6d81507defe6cfa1b68cf6214f7d62a180a
or1os <so1robboy0204@icloud.com>
2020-10-23 19:14:12 +0900

    サーバーアップロード2nd 5ndcommit

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
